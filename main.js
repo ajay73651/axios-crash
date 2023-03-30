c2fc994c8e6a6670f4b5958fda15323afc647787
ajay73651 <ajay73651@gmail.com>
2023-03-31 01:26:37 +0530

    updated file added

--- main.js
+++ main.js
@@ -1,51 +1,231 @@
+//AXIOS GLOBAL
+axios.defaults.headers.common['X-Auth-Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  //console.log('GET Request');
+  // axios({
+  //   method : "get",
+  //   url : "https://jsonplaceholder.typicode.com/todos",
+  //   params: {
+  //     _limit: 5
+  //   }
+  // })
+  // // .then(res => console.log(res.data))
+  // .then(res => showOutput(res))
+  // .catch(err => console.error(err));
+
+
+  // //2nd method for get same output
+  // axios.get("https://jsonplaceholder.typicode.com/todos",{
+  //   params: { _limit: 5 }
+  // })
+  // .then(res => showOutput(res))
+  // .catch(err => console.error(err));
+
+  //3rd method to set limit in another way
+  
+  //2nd method for get same output with setTimeOut limit
+  axios
+  .get("https://jsonplaceholder.typicode.com/todos?_limit=5" , { timeout: 5000})
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
+
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  //console.log('POST Request');
+  // axios({
+  //   method : "post",
+  //   url : "https://jsonplaceholder.typicode.com/todos",
+  //   data: {
+  //     title : "New todo",
+  //     completed : false
+  //   }
+  // })
+  // .then(res => showOutput(res))
+  // .catch(err => console.error(err));
+
+  //2nd way
+  axios
+  .post("https://jsonplaceholder.typicode.com/todos",{
+      title : "New todo",
+      completed : false
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
+
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  // console.log('PUT/PATCH Request');
+  // axios
+
+  //put :
+  // .put("https://jsonplaceholder.typicode.com/todos/1",{
+  //     title : "Updated todo",
+  //     completed : true
+  // })
+  // .then(res => showOutput(res))
+  // .catch(err => console.error(err));
+
+  
+  //patch :
+  axios
+  .patch("https://jsonplaceholder.typicode.com/todos/1",{
+      title : "Updated todo",
+      completed : true
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
+//note : diffrence between pu and patch
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  // console.log('DELETE Request');
+  axios
+  .delete("https://jsonplaceholder.typicode.com/todos/1",{
+      title : "Updated todo",
+      completed : true
+  })
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  // console.log('Simultaneous Request');
+  axios
+  .all([
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+  // .then(res=> {
+  //   console.log(res[0]);
+  //   console.log(res[1]);
+  //   showOutput(res[1]);
+  // })
+  //we can write like this too in smart way
+  .then(axios.spread((todos, posts) => showOutput(posts)))
+  .catch(err => console.log(err))
 }
 
-// CUSTOM HEADERS
+// CUSTSOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  // console.log('Custom Headers');
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken'
+    }
+  };
+
+  axios
+  .post(
+    'https://jsonplaceholder.typicode.com/todos' ,
+    {
+      title : "New todo",
+      completed : false
+    }, 
+  config
+  )
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
+
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  // console.log('Transform Response');
+  const options = {
+    method : 'post',
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    data: {
+     title: 'Hello world'
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data => {
+    data.title = data.title.toUpperCase();
+    return data
+  })
+};
+
+  axios(options).then(res => showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  // console.log('Error Handling');
+  axios
+  .get('https://jsonplaceholder.typicode.com/todoss', {
+    // validateStatus: function(status){
+    //   return status < 500; // reject if status is only greater or equal to 500 
+    // }
+  })
+  .then(res => showOutput(res))
+  .catch(err => {
+    if(err.response){
+      //server responded with the status other than 200 range 
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+      if(err.response.status == 404){
+        alert('Error: Page not found');
+      }
+    }else if(err.request){
+      //request was made but no response
+      console.error(err.request);
+    }else{
+      console.log(err.request);
+    }
+
+
+  })
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  // console.log('Cancel Token');
+  const source = axios.CancelToken.source();
+  axios
+  .get('https://jsonplaceholder.typicode.com/todos', {
+    cancelToken: source.token
+  })
+  .then(res => showOutput(res))
+  .catch(thrown => {
+    if(axios.isCancel(thrown)){
+      console.log('request cancelled', thrown.message)
+    }
+  })
+  if(true){
+    source.cancel('Request cancelled')
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+  config => {
+    console.log(
+      `${config.method.toUpperCase()} request sent to ${config.url} at
+       ${new Date().getTime()}`
+    );
+    return config
+  },
+  error =>{
+    return Promise.reject(error)
+  } 
+)
 
 // AXIOS INSTANCES
+const axiosInstance = axios.create({
+  //other custom settings 
+  baseURL : 'https://jsonplaceholder.typicode.com'
+});
+
+// axiosInstance.get('/comments').then(res => showOutput(res));
 
 // Show output in browser
 function showOutput(res) {
